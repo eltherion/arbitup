6ec12cde578706dbfbf7c720b7c572c9cf7a969b
eltherion <mateusz.murawski@datart.pl>
2021-01-11 22:03:06 -0500

    Project initialization.

---
+++ project/Dependencies.scala
@@ -0,0 +1,28 @@
+import sbt._
+
+
+object Dependencies {
+  private val ammonite        = "com.lihaoyi"                   %  "ammonite"         % VersionsOf.ammonite cross CrossVersion.full
+  private val betterFiles     = "com.github.pathikrit"          %% "better-files"     % VersionsOf.betterFiles
+  private val catsEffect      = "org.typelevel"                 %% "cats-effect"      % VersionsOf.catsEffect
+  private val janino          = "org.codehaus.janino"           %  "janino"           % VersionsOf.janino
+  private val logbackClassic  = "ch.qos.logback"                %  "logback-classic"  % VersionsOf.logbackClassic
+  private val monix           = "io.monix"                      %% "monix"            % VersionsOf.monix
+  private val pureConfig      = "com.github.pureconfig"         %% "pureconfig"       % VersionsOf.pureConfig
+  private val scalaLogging    = "com.typesafe.scala-logging"    %% "scala-logging"    % VersionsOf.scalaLogging
+  private val scalatest       = "org.scalatest"                 %% "scalatest"        % VersionsOf.scalatest      % Test
+  private val sttp            = "com.softwaremill.sttp.client3" %% "core"             % VersionsOf.sttp
+
+  val all = Seq(
+    ammonite,
+    betterFiles,
+    catsEffect,
+    janino,
+    logbackClassic,
+    monix,
+    pureConfig,
+    scalaLogging,
+    scalatest,
+    sttp
+  )
+}
---
+++ project/VersionsOf.scala
@@ -0,0 +1,13 @@
+object VersionsOf {
+  val ammonite        = "2.3.8-4-88785969"
+  val betterFiles     = "3.9.1"
+  val catsEffect      = "2.3.1"
+  val janino          = "3.1.2"
+  val logbackClassic  = "1.2.3"
+  val monix           = "3.3.0"
+  val pureConfig      = "0.14.0"
+  val scalaCache      = "0.27.0"
+  val scalaLogging    = "3.9.2"
+  val scalatest       = "3.2.3"
+  val sttp            = "3.0.0"
+}
